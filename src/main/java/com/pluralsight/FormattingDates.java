d74bc0a351e64db0199e8bc293cef192ef91e331
AliciaD02 <diazalicia4542@gmail.com>
2026-04-22 10:39:39 -0500

    I added more formats so I just copied and paste using different variables to store the output.

--- src/main/java/com/pluralsight/FormattingDates.java
+++ src/main/java/com/pluralsight/FormattingDates.java
@@ -1,4 +1,47 @@
 package com.pluralsight;
-
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 public class FormattingDates {
-}
+ // always start with a public static void main method
+  public static void main(String[] args) {
+      // because it is asking for the current date and time, we will use the LocalDateTime
+      //.now means it is for current time and date.
+
+    LocalDateTime date = LocalDateTime.now();
+
+    //we need to format the current date ("MM/dd/yyyy")
+      // after doing that we need to store the formatted date .
+      // currentDate is our variable name
+    DateTimeFormatter currentDate = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+    // we need to store the data in a string variable because its returning a date.
+      String formattedDate = date.format(currentDate);
+    //print out the formatted date
+    System.out.println("Today is" + " " + formattedDate);
+      //we need to format the current date in this method ("yyyy-MM-dd")
+      DateTimeFormatter currentDate2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+      //add a String variable to store the formatted date ("yyyy-MM-dd")
+      String currentFormatter = date.format(currentDate2);
+      //print out the formatted date
+      System.out.println("Today is" + " " + currentFormatter);
+
+
+      //we need to format the current date in this method ("EEEE dd, yyyy")
+      DateTimeFormatter currentDate3 = DateTimeFormatter.ofPattern("EEEE dd, yyyy");
+      //add a String variable to store it
+      String currentFormatter3 = date.format(currentDate3);
+      //print date
+      System.out.println("Today is" + " " + currentFormatter3);
+
+       //format ("EEEE, MMM  dd, yyyy hh:mm  ")
+      DateTimeFormatter currentDate4 = DateTimeFormatter.ofPattern("EEEE, MMM  dd, yyyy hh:mm  ");
+      //add the string variable to store it
+      String currentFormatter4 = date.format(currentDate4);
+              //print date
+      System.out.println("Today is" + " " + currentFormatter4);
+
+  }
+
+
+    }
+
